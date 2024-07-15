4d69627edf659fd33622a7a5252d498139eeb259
Alexander Dinges <alexding97@gmail.com>
2024-07-15 22:23:39 +0200

    240715

--- framework_laptop.nix
+++ framework_laptop.nix
@@ -27,9 +27,8 @@
 #    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
 #  };
   # Hardware acceleration
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport = true;
     extraPackages = with pkgs; [
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
       vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
